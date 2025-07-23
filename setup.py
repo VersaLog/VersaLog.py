ea5e757bb03dc94b8fe5e8f9748b32e477aedc57
kayu0514 <kunka6066@gmail.com>
2025-07-23 10:48:32 +0900

    change to testcode | VersaLog version up.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="VersaLog",
-    version="1.3.8",
+    version="1.4.2",
     description="Versatile logging library.",
     long_description=long_description,
     long_description_content_type="text/markdown",
--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,7 +1,7 @@
 from VersaLog import *
 
 # show_file False
-logger = VersaLog(mode="simple", show_file=False)
+logger = VersaLog(mode="simple", show_file=False, show_tag=False, notice=False)
 logger.info("ok")
 logger.error("err")
 logger.warning("war")
@@ -9,7 +9,7 @@ logger.debug("deb")
 logger.critical("cri")
 
 # show_file True
-logger = VersaLog(mode="simple", show_file=True)
+logger = VersaLog(mode="simple", show_file=True, show_tag=False, notice=False)
 logger.info("ok")
 logger.error("err")
 logger.warning("war")
@@ -17,7 +17,7 @@ logger.debug("deb")
 logger.critical("cri")
 
 # show_tag False
-logger = VersaLog(mode="simple", show_tag=False)
+logger = VersaLog(mode="simple", show_file=False, show_tag=False, notice=False)
 logger.info("ok")
 logger.error("err")
 logger.warning("war")
@@ -25,15 +25,31 @@ logger.debug("deb")
 logger.critical("cri")
 
 # show_tag True
-logger = VersaLog(mode="simple", show_tag=True, tag="VersaLog")
+logger = VersaLog(mode="simple", show_file=False, show_tag=True, notice=False, tag="VersaLog")
 logger.info("ok")
 logger.error("err")
 logger.warning("war")
 logger.debug("deb")
 logger.critical("cri")
 
-# all True
-logger = VersaLog(mode="simple", all=True)
+# notice False
+logger = VersaLog(mode="detailed", show_file=False, show_tag=False, notice=False)
+logger.info("ok")
+logger.error("err")
+logger.warning("war")
+logger.debug("deb")
+logger.critical("cri")
+
+# notice True
+logger = VersaLog(mode="detailed", show_file=False, show_tag=False, notice=True)
+logger.info("ok")
+logger.error("err")
+logger.warning("war")
+logger.debug("deb")
+logger.critical("cri")
+
+# enable_all True
+logger = VersaLog(mode="simple", enable_all=True)
 logger.info("ok")
 logger.error("err")
 logger.warning("war")
