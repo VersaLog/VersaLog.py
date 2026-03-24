b725d665d780612baba4afecc4161b7f4649cb43
kaedeek <yukise30@icloud.com>
2026-03-24 15:39:19 +0900

    feat(change): __init__ and festapi file.

--- VersaLog/integrations/fastapi.py
+++ VersaLog/integrations/fastapi.py
@@ -1,13 +1,8 @@
 import logging
 from ..handler import VersaLogHandler
 
-
 def setup_fastapi_logging(versalog):
     handler = VersaLogHandler(versalog)
-
-    handler.setFormatter(
-        logging.Formatter("[%(asctime)s][%(levelname)s] %(message)s")
-    )
 
     root = logging.getLogger()
 
@@ -17,17 +12,11 @@ def setup_fastapi_logging(versalog):
     root.setLevel(logging.NOTSET)
     root.addHandler(handler)
 
-    for name in [
-        "uvicorn",
-        "uvicorn.error",
-        "uvicorn.access",
-        "asyncio"
-    ]:
+    for name in ["uvicorn", "uvicorn.error", "uvicorn.access", "asyncio"]:
         logger = logging.getLogger(name)
 
         for h in logger.handlers[:]:
             logger.removeHandler(h)
 
         logger.propagate = True
-
         logger.setLevel(logging.NOTSET)
