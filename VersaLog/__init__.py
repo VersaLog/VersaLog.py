dbdf22b0275dddde691484db4132eb154e31166a
kaedeek <yukise30@icloud.com>
2026-04-15 20:12:22 +0900

    feat: add auto pyfile.

--- VersaLog/__init__.py
+++ VersaLog/__init__.py
@@ -1,5 +1,6 @@
 from .core import VersaLog
 from .handler import VersaLogHandler
+from .integrations.auto import auto_setup
 from .integrations.fastapi import setup_fastapi_logging
 from .integrations.flask import setup_flask_logging
 from .integrations.django import setup_django_logging
@@ -9,4 +10,4 @@ __version__ = "2.6.0"
 __author__ = "Kayu0514"
 __url__ = "https://github.com/kayu0514"
 
-__all__ = ["VersaLog", "VersaLogHandler", "setup_fastapi_logging","setup_flask_logging", "setup_django_logging"]
+__all__ = ["VersaLog", "VersaLogHandler", "auto_setup", "setup_fastapi_logging","setup_flask_logging", "setup_django_logging"]
