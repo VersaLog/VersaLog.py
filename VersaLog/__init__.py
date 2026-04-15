ea5c5bdf08695646940f4e545c6d763dba2745c8
kaedeek <yukise30@icloud.com>
2026-04-15 20:07:26 +0900

    feat(Update): Update

--- VersaLog/__init__.py
+++ VersaLog/__init__.py
@@ -1,10 +1,12 @@
 from .core import VersaLog
 from .handler import VersaLogHandler
 from .integrations.fastapi import setup_fastapi_logging
+from .integrations.flask import setup_flask_logging
+from .integrations.django import setup_django_logging
 
 __copyright__ = "Copyright 2025 by Kayu0514"
-__version__ = "2.5.6"
+__version__ = "2.6.0"
 __author__ = "Kayu0514"
 __url__ = "https://github.com/kayu0514"
 
-__all__ = ["VersaLog", "VersaLogHandler", "setup_fastapi_logging"]
+__all__ = ["VersaLog", "VersaLogHandler", "setup_fastapi_logging","setup_flask_logging", "setup_django_logging"]
