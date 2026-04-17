a356134bf3f08c122ae48670e28ea8575710b132
fuchan12040502-innu <fuchan12040502@gmail.com>
2026-04-18 02:16:14 +0900

    feat: implement Django logging integration and add example

--- VersaLog/integrations/django.py
+++ VersaLog/integrations/django.py
@@ -1,5 +1,19 @@
-from .generic import setup_logging
+import logging
+from ..handler import VersaLogHandle
 
 
 def setup_django_logging(versalog):
-    setup_logging(versalog)
+    handler = VersaLogHandler(versalog)
+
+    root = logging.getLogger()
+    for h in root.handlers[:]:
+        root.removeHandler(h)
+    root.setLevel(logging.NOTSET)
+    root.addHandler(handler)
+
+    for name in ["django", "django.request", "django.server", "django.db.backends"]:
+        logger = logging.getLogger(name)
+        for h in logger.handlers[:]:
+            logger.removeHandler(h)
+        logger.propagate = True
+        logger.setLevel(logging.NOTSET)
